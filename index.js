15342fc1e37506c8e1a6520361589cb289fbd9dc
Tim <mohamud.hussein@student.moringaschool.com>
2023-06-16 22:07:23 +0300

    first assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let numbers = [1, 7, 12, 20, 5];
+let max = Math.max(...numbers);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max,
+};
+
